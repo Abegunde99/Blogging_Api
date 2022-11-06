7d333a769a7855e6cb61ee7990619e6d55fadc1f
Abegunde99 <preciousolanrewaju1998@gmail.com>
2022-11-06 22:55:49 +0100

    I made some changes to my app.js file

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const User = require('./src/models/UserModel');
-const Post = require('./src/models/PostModel');
 const userRouter = require('./src/routes/userRoutes');
 const postRouter = require('./src/routes/postRoutes')
 const { checkUser } = require('./middlewares/auth');
@@ -15,7 +14,7 @@ const app = express()
 
 //middleWares
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
 app.use(cookieParser());
@@ -23,16 +22,7 @@ app.use(cookieParser());
 
 //routes
 app.use(checkUser);
-app.get('/', async (req,res, next )=>{
-    await Post.find({state:"published"}, (err, posts) => {
-        if (err) {
-            next(err)
-        }
-        else {
-
-            res.jlcome to the home page", posts})
-        }
-}); //home route
+app.get('/', (req,res )=>{res.send('Welcome to my blog')}); //home route
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/posts', postRouter);
 app.get('*', (req, res) => {
